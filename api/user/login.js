b399b1fd944d3393e45f7b0839fc924a35a060e6
JJuOn <jwseo001@khu.ac.kr>
2021-04-15 21:35:08 +0900

    Implement login

--- api/user/login.js
+++ api/user/login.js
@@ -1,3 +1,4 @@
+const crypto=require('crypto');
 const User=require('../../models/user');
 
 const Login=(req,res)=>{
@@ -15,11 +16,56 @@ const Login=(req,res)=>{
         });
     }
 
-    const getUserInfo=()=>{
-        return new Promise((resolve,reject)=>{
-
+    const GetUserInfo=()=>{
+        return new Promise(async (resolve,reject)=>{
+            try{
+                const user=await User.findOne({userId:userId});
+                if(!user){
+                    reject();
+                }
+                else{
+                    resolve(user);
+                }
+            }
+            catch(e){
+                reject(e);
+            }
         });
     }
+
+    const CheckPassword=(user)=>{
+        return new Promise((resolve,reject)=>{
+            const key=crypto.pbkdf2Sync(password,user.salt,process.env.REPEAT_NUM,64,'sha512');
+            if(key.toString('base64')===user.hashedPassword){
+                resolve(user);
+            }
+            else{
+                reject();
+            }
+        });
+    }
+
+    const JWTUpdate=(user)=>{
+        return new Promise((resolve,reject)=>{
+            const token=user.generateToken();
+            res.cookie("token",token);
+            resolve();
+        });
+    }
+
+    DataCheck()
+    .then(GetUserInfo)
+    .then(CheckPassword)
+    .then(JWTUpdate)
+    .then(()=>{
+        res.status(200);
+    })
+    .catch((e)=>{
+        console.error(e);
+        res.status(500);
+    });
 }
+
+
 
 module.exports=Login;
